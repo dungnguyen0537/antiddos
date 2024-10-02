1ed4bea803c68d3a8373024d9f44cfe4b3384477
dungnguyen0537 <dungnguyen120737@gmail.com>
2024-10-02 15:36:38 +0700

    Update get-rules.sh

--- get-rules.sh
+++ get-rules.sh
@@ -1,4 +1,4 @@
 curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/4303f61c7dd2170fc9f8525fa23013d9/firewall/rules" \
+"https://api.cloudflare.com/client/v4/zones/f45118cd00ea0245a6f7e9b80ad5c7d3/firewall/rules" \
 -H "X-Auth-Email: dungnguyen120737@gmail.com" \
 -H "X-Auth-Key: 89bba666acac57579b5a48189eeb4013d2b9d"
