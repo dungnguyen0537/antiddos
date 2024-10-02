1bf6a62e582eb1d9f68146ba4a9e953448ecbc13
dungnguyen0537 <dungnguyen120737@gmail.com>
2024-10-02 15:37:21 +0700

    Update captcha-on.sh

--- captcha-on.sh
+++ captcha-on.sh
@@ -1,4 +1,4 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/4303f61c7dd2170fc9f8525fa23013d9/firewall/rules/b617d312e7284e739daab459a7ed7782" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/f45118cd00ea0245a6f7e9b80ad5c7d3/firewall/rules/b617d312e7284e739daab459a7ed7782" \
      -H "X-Auth-Email: dungnguyen120737@gmail.com" \
      -H "X-Auth-Key: 89bba666acac57579b5a48189eeb4013d2b9d" \
      -H "Content-Type: application/json" \
