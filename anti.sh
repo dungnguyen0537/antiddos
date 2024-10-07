c5204369016dc6b881e80b3017b3d2fd205994e2
dungnguyen0537 <dungnguyen120737@gmail.com>
2024-10-08 00:42:51 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,6 +1,6 @@
 #!bin/sh
-connect=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 150 ]
+connect=$(netstat -tun | grep ESTABLISHED | wc -l)
+if [ $connect -lt 200 ]
 then
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/dungnguyen0537/antiddos/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/dungnguyen0537/antiddos/main/captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
