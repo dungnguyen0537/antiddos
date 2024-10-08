60e46cdb58234aa26856a77b2fdad0e8819f5c84
dungnguyen0537 <dungnguyen120737@gmail.com>
2024-10-08 16:36:24 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -2,7 +2,7 @@
 connect1=$(netstat -tun | grep ESTABLISHED | wc -l)
 connect2=$(netstat -alntp | grep :443 | wc -l)
 
-if [ $connect1 -gt 100 ] || [ $connect2 -gt 100 ]; then
+if [ $connect1 -gt 150 ] || [ $connect2 -gt 150 ]; then
   cat /root/status.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/dungnguyen0537/antiddos/main/block-on.sh) && bash <(curl -Ls https://raw.githubusercontent.com/dungnguyen0537/antiddos/main/captcha-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
 else
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/dungnguyen0537/antiddos/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/dungnguyen0537/antiddos/main/captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
